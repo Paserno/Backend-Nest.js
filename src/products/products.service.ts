668d4b4dea2ba3d558bbdc88b2bd22a8826484b4
Paserno <paserno.x@gmail.com>
2023-06-02 01:34:31 -0400

    Paginacion en TypeORM

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 import { Product } from './entities/product.entity';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class ProductsService {
@@ -22,19 +23,6 @@ export class ProductsService {
   async create(createProductDto: CreateProductDto) {
 
     try {
-
-      // if (!createProductDto.slug) {
-      //   createProductDto.slug = createProductDto.title
-      //     .toLowerCase()
-      //     .replaceAll(' ', '_')
-      //     .replaceAll("'", '')
-      // } else {
-      //   createProductDto.slug = createProductDto.slug
-      //   .toLowerCase()
-      //   .replaceAll(' ', '_')
-      //   .replaceAll("'", '')
-      // }
-
       const product = this.productRepository.create(createProductDto);
       await this.productRepository.save(product);
 
@@ -43,25 +31,37 @@ export class ProductsService {
     } catch (error) {
       this.handleDBExceptions(error);
     }
-
-
-
   }
 
-  findAll() {
-    return `This action returns all products`;
+
+  findAll(paginationDto: PaginationDto) {
+
+    const { limit = 10, offset = 0} = paginationDto;
+
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+      //TODO: Relaciones
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.productRepository.findOneBy({id});
+    if ( !product ){
+      throw new NotFoundException(`Product with id '${id}' not found`)
+    }
+    
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const product = await this.findOne(id);
+    await this.productRepository.remove(product);
+    return product;
   }
 
 
